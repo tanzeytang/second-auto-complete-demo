99e309a1d7dd5ed282aa9efd281810a2bc192e04
择星 <45704419+tanzeytang@users.noreply.github.com>
2019-10-02 23:56:22 +1000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = require("./routes/router");
 require("dotenv").config();
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT || 3001
 
 app.use(router);
 
